143a287135aad4fc4f8e9c736c9f081210cf1629
Alfredo Prades <aprades@gmail.com>
2022-09-10 02:25:14 +0200

    Funcionamiento correcto

--- ASCReader.py
+++ ASCReader.py
@@ -4,7 +4,6 @@
 # This AMF ASC parser is based on the AMF ASC parser in legacy cura:
 # https://github.com/daid/LegacyCura/blob/ad7641e059048c7dcb25da1f47c0a7e95e7f4f7c/Cura/util/meshLoaders/asc.py
 
-from tkinter.tix import INTEGER
 from UM.MimeTypeDatabase import MimeTypeDatabase, MimeType
 from cura.CuraApplication import CuraApplication
 from UM.Logger import Logger
@@ -103,14 +102,17 @@ class ASCReader(MeshReader):
                 if elev_point == nodata:
                     vertex[2] = 0.0  #O base elevation
                 else:
-                    vertex[2] = float(elev_point)
-                    if vertex[2] > max_elevation: 
-                        max_elevation = vertex[2]
-                    elif vertex[2] < min_elevation:
-                        min_elevation = vertex[2]
+                    try:
+                        vertex[2] = float(elev_point)
+                        if vertex[2] > max_elevation: 
+                            max_elevation = vertex[2]
+                        elif vertex[2] < min_elevation:
+                            min_elevation = vertex[2]
+                    except:
+                        continue
 
 
-                Logger.log("i",str(vertex))        
+                #Logger.log("i",str(vertex))        
                 asc_mesh.append(vertex)
                 x+=1
             y+=1
@@ -122,87 +124,107 @@ class ASCReader(MeshReader):
         if min_elevation > elevation_range:
             base_z = min_elevation - elevation_range
         
-   
 
-        elevation_factor = 30
+        ncols = int(self.header['NCOLS'])
+        nrows = int(self.header['NROWS'])
+        Logger.log("i","NROWS'%s'"%nrows)
+
+
+
+        elevation_factor = 1
         ok_cellsize = elevation_factor * cellsize
+        # top
         for x in range(0,ncols):
             vertex = [x * ok_cellsize, 0.0, base_z]
             asc_mesh.append(vertex)
+            #Logger.log("d","TOP vertex %s %s currenLen: %s"%(x,vertex,len(asc_mesh)))
 
+        # right
         right_x = (ncols -1) * ok_cellsize
         for y in range(0,nrows):
             vertex = [right_x, y * ok_cellsize, base_z]
             asc_mesh.append(vertex)
+            #Logger.log("d","RIGHT vertex %s %s currenLen: %s"%(y,vertex,len(asc_mesh)))
             
+        # bottom    
         bottom_y = (nrows -1) * ok_cellsize
         for x in range(0,ncols):
             vertex= [x * ok_cellsize, bottom_y, base_z]
             asc_mesh.append(vertex)
+            #Logger.log("d","BOTTOM vertex %s %s currenLen: %s"%(x,vertex,len(asc_mesh)))
 
+        # left 
         for y in range(0,nrows):
             vertex= [0 , y * ok_cellsize, base_z]
             asc_mesh.append(vertex)
+            #Logger.log("d","LEFT vertex %s %s currenLen: %s"%(y,vertex,len(asc_mesh)))
 
 
         
         Logger.log("i","POINTS")
-        Logger.log("i",str(asc_mesh))
+        #Logger.log("i",str(asc_mesh))
 
-        #Top vertex
-        ncols = int(self.header['NCOLS'])
-        nrows = int(self.header['NROWS'])
-        Logger.log("i","NROWS'%s'"%nrows)
+        
+
         asc_mesh_triangles = []
+        offset = ncols * nrows 
+        offset_last_row = offset - ncols
+        offset_bottom  = offset + ncols + nrows
+        
+        #Top vertex
         for j in range(1,nrows):   
             id_behind = (j -1) * ncols
-            for i in range(1,ncols):
+            for i in range(0,ncols-1):
                 my_id = id_behind + i
+                #Logger.log("d","i j  vertex %s %s - myid: %s"%(i,j,my_id))
                 asc_mesh_triangles.append([my_id , my_id + 1 , my_id + 1 + ncols])
                 asc_mesh_triangles.append([my_id , my_id + 1 + ncols ,my_id + ncols])
         
-        
-        offset = ncols * nrows
-        offset_last_row = offset - ncols
-        offset_bottom  = offset + ncols + nrows
-        for i in range(1,ncols):
+
+
+        Logger.log("d","triangles top end")
+        for i in range(0,ncols-1):
+            #Logger.log("d","top bottom i vertex %s"%(i))
             #top wall
             asc_mesh_triangles.append([i, i + offset,i + 1])
             asc_mesh_triangles.append([i +1 , i + offset, i + offset + 1])
 
             #bottom wall    
             asc_mesh_triangles.append([i + offset_last_row + 1 , i + offset_bottom, i + offset_last_row])
-            asc_mesh_triangles.append([i + offset_last_row + 1 , i + offset_bottom, i + offset_last_row + 1])
-             
+            asc_mesh_triangles.append([i + offset_bottom + 1 , i + offset_bottom, i + offset_last_row + 1])
 
-        start_right = offset + ncols
+        Logger.log("i","VERTEX walls half")    
+        start_right = offset + ncols 
         start_left = offset + ncols * 2 + nrows
-        for j in range(1,nrows):
+        for j in range(0,nrows-1):
+            #Logger.log("d","right left  j vertex %s"%(j))
             #right wall
-            asc_mesh_triangles.append([ncols * ( j + 1 ), ncols * j, start_right + j ])
-            asc_mesh_triangles.append([ncols * ( j + 1 ), ncols + j, start_right + j + 1 ])
-
+            asc_mesh_triangles.append([ncols * ( j + 2 ) -1 , ncols * (j + 1) -1 , start_right + j ])
+            asc_mesh_triangles.append([ncols * ( j + 2 ) -1, start_right + j , start_right + j +1 ])
             #left wall
-            asc_mesh_triangles.append([start_left + j, ncols * ( j - 1 ), ncols * j + 1])
-            asc_mesh_triangles.append([start_left + j + 1, start_left + j , ncols * j + 1])
+            asc_mesh_triangles.append([start_left + j, ncols * j , ncols * (j + 1)])
+            asc_mesh_triangles.append([start_left + j + 1, start_left + j , ncols * (j + 1)])
 
-        
+         
         # Bottom triangles
-        p1 = ncols * nrows + 1
-        p2 = p1 + nrows - 1
-        p4 = p2 + ncols - 1  
-        p3 = p4 + nrows - 1
+        p1 = offset 
+        p2 = p1 + ncols
+        p3 = p2 + nrows 
+        p4 = p3 + ncols -1
 
         asc_mesh_triangles.append([p3,p2,p1])
         asc_mesh_triangles.append([p3,p4,p2])
-
-        Logger.log("i","VERTEX")
-        Logger.log("i",str(asc_mesh_triangles))
+        Logger.log("i","VERTEX end")
+        #Logger.log("i",str(asc_mesh_triangles))
         
         mesh = trimesh.base.Trimesh(vertices = numpy.array(asc_mesh, dtype = numpy.float32), faces = numpy.array(asc_mesh_triangles, dtype = numpy.int32))
+        Logger.log("i","Mesh built")
         mesh.merge_vertices()
+        Logger.log("i","merge_vertices")
         mesh.remove_unreferenced_vertices()
+        Logger.log("i","remove unreferenced")
         mesh.fix_normals()
+        Logger.log("i","fix_normals")
         mesh_data = self._toMeshData(mesh, file_name)
 
         nodes=[]
@@ -260,7 +282,9 @@ class ASCReader(MeshReader):
 
         vertices = numpy.asarray(vertices_list, dtype = numpy.float32)
         indices = numpy.asarray(indices_list, dtype = numpy.int32)
+        Logger.log("i","calculate Normals")
         normals = calculateNormalsFromIndexedVertices(vertices, indices, face_count)
+        Logger.log("i","calculate Normals end")
 
         mesh_data = MeshData(vertices = vertices, indices = indices, normals = normals,file_name = file_name)
         return mesh_data
